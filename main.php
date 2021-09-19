c4eeec41490d723174019d3be532524eb9c3cbf6
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2021-09-19 13:32:16 +0930

    update main to have makerspace logo update en flag to aus flag update form to match makerspace req

--- main.php
+++ main.php
@@ -85,147 +85,44 @@ echo "<title>$companyname - $TEXT_VISITOR_BADGE_TITLE</title>"; ?>
     $( "#vehicleonsiteradio" ).buttonset();
   });
   </script>
-<?php
-if($bEnableVirtualKeyboard === true)
-{
-echo('
-<!-- keyboard widget css & script (required) -->
-<link href="virtualkeyboard/css/keyboard.min.css" rel="stylesheet">
-<script type="text/javascript" src="virtualkeyboard/js/jquery.keyboard.min.js" charset="utf-8"></script>
-<script type="text/javascript" src="virtualkeyboard/layouts/alphasmall.js" charset="utf-8"></script>
-');
-}
-?>
 
 <!-- <link rel="stylesheet" type="text/css" href="visitors.css"> -->
+
 <script type="text/javascript" src="validation.js"></script>
 <SCRIPT type="text/javascript">
 //client side validation checking
-function CheckFirstName(sFirstName)
+function CheckName(sName)
 {
   var sError;
   sError = "";
   
-  if(sFirstName == "")
+  if(sName == "")
   {
-    sError = "<?php echo $TEXT_PLEASE_ENTER_YOUR_FIRST_NAME ?>\n";
+    sError = "<?php echo $TEXT_PLEASE_ENTER_YOUR_FULL_NAME ?>\n";
   }
   
   return sError;
 }
 
-function CheckLastName(sLastName)
+function CheckNumber(sNumber)
 {
   var sError;
   sError = "";
   
-  if(sLastName == "")
+  if(sNumber == "")
   {
-    sError = "<?php echo $TEXT_PLEASE_ENTER_YOUR_LAST_NAME ?>\n";
+    sError = "<?php echo $TEXT_PLEASE_ENTER_YOUR_NUMBER ?>\n";
   }
   
   return sError;
-}
-
-function CheckCompanyName(sCompanyName)
-{
-  var sError;
-  sError = "";
-  
-  if(sCompanyName == "")
-  {
-    sError = "<?php echo $TEXT_PLEASE_ENTER_YOUR_COMPANY_NAME ?>\n";
-  }
-  
-  return sError;
-}
-
-<?php
-if($bEnableJoomlaContactsSupport === true)
-{
-echo "
-function CheckReasonForVisit(sReasonForVisit)
-{
-  var sError;
-  sError = \"\";
-  
-  if(sReasonForVisit == \"\" || sReasonForVisit == \"-1\")
-  {
-    sError = \"$TEXT_PLEASE_SELECT_EMPLOYEE_OR_SERVICE_CONCERNED\\n\";
-  }
-  
-  return sError;
-}
-";
-}
-else
-{
-  echo "
-function CheckReasonForVisit(sReasonForVisit)
-{
-  var sError;
-  sError = \"\";
-  
-  if(sReasonForVisit == \"\")
-  {
-    sError = \"$TEXT_PLEASE_ENTER_REASON_FOR_VISIT\\n\";
-  }
-  
-  return sError;
-}
-";
-}
-?>
-
-function CheckVehicleOnSite(rVehicleOnSite)
-{
-  var sError;
-  sError = "";
-  
-  if(rVehicleOnSite[0].checked == false && rVehicleOnSite[1].checked == false)
-  {
-    sError = "<?php echo $TEXT_PLEASE_INDICATE_WHETHER_YOUR_VEHICLE_IS_ON_THE_SITE ?>\n";
-  }
-  
-  return sError;
-}
-
-function CheckLicensePlate(sLicensePlate)
-{
-  var sError;
-  sError = "";
-  
-  if(sLicensePlate == "")
-  {
-    sError = "<?php echo $TEXT_PLEASE_ENTER_YOUR_LICENSE_PLATE_NUMBER ?>\n";
-  }
-  
-  return sError;
-}
-
-function OnVehicleOnSiteValueChanged(fForm)
-{
-  if(fForm.vehicleonsite[0].checked)
-  {
-	document.getElementById("licenseplatenumberlabel").innerHTML ="<b><?php echo "$TEXT_LICENSE_PLATE_NUMBER" ?><\/b>";
-	fForm.licenseplate.disabled=false;
-  }
-  else
-  {
-    document.getElementById("licenseplatenumberlabel").innerHTML ="<?php echo "$TEXT_LICENSE_PLATE_NUMBER" ?>";
-	fForm.licenseplate.value="";
-	fForm.licenseplate.disabled=true;
-  }
-  
-  return true;
 }
 
 function checkWholeForm(theForm) {
     var why = "";
     //why += checkUsername(theForm.vname.value);
-	why += CheckFirstName(theForm.firstname.value);
+	why += CheckName(theForm.name.value);
 	why += CheckLastName(theForm.lastname.value);
-    why += CheckCompanyName(theForm.company.value);
+    why += CheckNumber(theForm.number.value);
     why += CheckReasonForVisit(theForm.visiting.value);
 	why += CheckVehicleOnSite(theForm.vehicleonsite);
 	if(theForm.vehicleonsite[0].checked)
@@ -251,7 +148,7 @@ window.location=page;
 <div id="warning"><script type="text/javascript"><!-- // --> </script>
 <noscript><p>$TEXT_JAVASCRIPT_SUPPORT_REQUIRED</p></noscript></div>
 <div class="visitors">
-<h1><?php echo "$TEXT_WELCOME_TO $shortname"?></h1>
+<img width="200" src="images/makerspacelogo.svg" alt="Makerspace" />
 <h2><?php echo "$TEXT_VISITOR_PLEASE_SIGN_IN"?></h2>
 </div>
 <div id="accordion">
@@ -259,7 +156,7 @@ window.location=page;
 /* BEGIN translations support section */
 echo "<h3>".$TEXT_CHOOSE_YOUR_LANGUAGE."</h3>";
 echo "<div>";
-echo "<div class=\"centered-flow\">";
+echo "<div class=\"centered-flow-block languages\">";
 
 $dDirectory = dir("./languages/");
 
@@ -267,7 +164,7 @@ while (false !== ($sEntry = $dDirectory->read()))
 {
    if($sEntry !== "." && $sEntry !== "..")
    { 
-     echo "<div class=\"centered-flow-block\"><a href=\"".basename($_SERVER["PHP_SELF"])."?language=".$sEntry."\">".$sEntry."<br><img src=\"languages/".$sEntry."/lang.png\"/></a></div>";
+     echo "<a href=\"".basename($_SERVER["PHP_SELF"])."?language=".$sEntry."\">".$sEntry."<br><img src=\"languages/".$sEntry."/lang.png\"/></a>";
    }
 }
 $dDirectory->close();
@@ -277,204 +174,57 @@ echo "</div>";
 ?>
 <h3><?php echo $TEXT_ENTRANCE ?></h3>
 <div>
-<div class="centered-flow">
 <div class="centered-flow-block">
-<form action="process.php?language=<?php echo $sLanguage ?>" autocomplete="off" method="post" name="newvisitor">
+<form class="form" action="process.php?language=<?php echo $sLanguage ?>" autocomplete="off" method="post" name="newvisitor">
 <!--<form action="process.php?language=<?php echo $sLanguage ?>" method="post" name="newvisitor">-->
-	<div class="centered-table">
-		<div class="table-row">
-			<div class="table-cell-right-align">
-				<b><?php echo "$TEXT_FIRST_NAME"?></b>
-			</div>
-			<div class="table-cell-left-align">
-				<input id="firstnameinternational" type="text" name="firstname" autocomplete="off" placeholder="<?php echo "$TEXT_ENTER_YOUR_FIRST_NAME";?>" maxlength="255">
-<?php
-if($bEnableVirtualKeyboard === true)
-{
-echo '
-				<script type="text/javascript">
-				$(\'#firstnameinternational\')
-	.keyboard({ layout: \'alphasmall\', autoAccept: true, tabNavigation: true, enterNavigation: true })
-	.addTyping();
-	</script>
-';
-}
-?>
-			</div>
+	<div class="table-row">
+		<div class="table-cell-right-align">
+			<b><?php echo "$TEXT_FULL_NAME"?></b>
 		</div>
-		<div class="table-row">
-			<div class="table-cell-right-align">
-				<b><?php echo "$TEXT_LAST_NAME"?></b>
-			</div>
-			<div class="table-cell-left-align">
-				<input id="lastnameinternational" type="text" name="lastname" autocomplete="off" placeholder="<?php echo "$TEXT_ENTER_YOUR_LAST_NAME";?>" maxlength="255">
-<?php
-if($bEnableVirtualKeyboard === true)
-{
-echo '
-				<script type="text/javascript">
-				$(\'#lastnameinternational\')
-	.keyboard({ layout: \'alphasmall\', autoAccept: true, tabNavigation: true, enterNavigation: true })
-	.addTyping();
-	</script>
-';
-}
-?>
-			</div>
-		</div>
-		<div class="table-row">
-			<div class="table-cell-right-align">
-				<b><?php echo "$TEXT_COMPANY"?></b>
-			</div>
-			<div class="table-cell-left-align">
-				<input id="companyinternational" type="text" name="company" autocomplete="off" placeholder="<?php echo "$TEXT_ENTER_YOUR_COMPANY_NAME";?>" maxlength="255">
-<?php
-if($bEnableVirtualKeyboard === true)
-{
-echo '
-				<script type="text/javascript">
-				$(\'#companyinternational\')
-	.keyboard({ layout: \'alphasmall\', autoAccept: true, tabNavigation: true, enterNavigation: true })
-	.addTyping();
-	</script>
-';
-}
-?>
-			</div>
-		</div>
-		<?php
-			if($bEnableJoomlaContactsSupport === true)
-			{
-				echo "
-		<div class=\"table-row\">
-			<div class=\"table-cell-right-align\">
-			<div class=\"plain-centered-block\">
-				<b>$TEXT_EMPLOYEE_OR_SERVICE_CONCERNED</b>
-			</div>
-			</div>
-			<div class=\"table-cell-left-align\">
-				<select id=\"visitingselect\" name=\"visiting\">
-				<option selected value=\"-1\">$TEXT_PLEASE_SELECT</option>
-				";
-				require 'database.php';
-					
-				$sRequest = "SELECT `id`, ".$sJoomlaValueToUseAsEmployeeOrServiceConcerned." AS nameandphone FROM `".$sJoomlaDataBase."`.`".$sJoomlaTablesPrefix.$sJoomlaContactDetailsTable."`WHERE `published` = 1 AND `catid` IN (".$sJoomlaContactCategoryIDs.") AND `misc` NOT LIKE '%".$sJoomlaOmitContactsWithThisTagInMisc."%' ORDER BY `catid` DESC,`name` ASC";
-				$qResults = mysqli_query($database_link, $sRequest);
-					
-				while($rRow = mysqli_fetch_array($qResults, MYSQLI_ASSOC))
-				{
-					echo '<option value="'.$rRow['nameandphone'].'">'.$rRow['nameandphone'].'</option>'."\n";
-				}
-				
-				mysqli_free_result($qResults);
-				mysqli_close($database_link);
-				
-				echo "
-				</select>
-				<script type=\"text/javascript\">
-    $( \"#visitingselect\" ).selectmenu({width:\"105%\"})
-	.selectmenu( \"menuWidget\" )
-    .addClass( \"overflow\" );
-  </script>
-			</div>
-		</div>
-				";	
-			}
-			else
-			{
-				echo "
-		<div class=\"table-row\">
-			<div class=\"table-cell-right-align\">
-				<b>$TEXT_REASON_FOR_VISIT</b>
-			</div>
-			<div class=\"table-cell-left-align\">
-				<input id=\"visitinginternational\" type=\"text\" name=\"visiting\" autocomplete=\"off\" placeholder=\"$TEXT_ENTER_REASON_FOR_VISIT\" maxlength=\"255\">
-";
-if($bEnableVirtualKeyboard === true)
-{
-echo "
-				<script type=\"text/javascript\">
-				$('#visitinginternational')
-	.keyboard({ layout: 'alphasmall', autoAccept: true, tabNavigation: true, enterNavigation: true })
-	.addTyping();
-	</script>
-";
-}
-echo "
-			</div>
-		</div>
-				";
-			}
-		?>
-		
-		<div class="table-row">
-			<div class="table-cell-right-align">
-				<b><?php echo "$TEXT_VEHICLE_ON_SITE" ?></b>
-			</div>
-			<div class="table-cell-left-align">
-				<div id="vehicleonsiteradio">
-				<input type="radio" id="vehicleonsiteradio1" name="vehicleonsite" value="1" onchange="OnVehicleOnSiteValueChanged(newvisitor)"><label for="vehicleonsiteradio1"><?php echo $TEXT_YES ?></label>
-				<input type="radio" id="vehicleonsiteradio2" name="vehicleonsite" value="0" onchange="OnVehicleOnSiteValueChanged(newvisitor)"><label for="vehicleonsiteradio2"><?php echo $TEXT_NO ?></label>
-				</div>
-			</div>
-		</div>
-		<div class="table-row">
-			<div class= "table-cell-right-align" id="licenseplatenumberlabel">
-				<?php echo "$TEXT_LICENSE_PLATE_NUMBER" ?>
-			</div>
-			<div class="table-cell-left-align">
-				<input id="licenseplateinternational" type="text" name="licenseplate" autocomplete="off" placeholder="<?php echo "$TEXT_ENTER_YOUR_LICENSE_PLATE_NUMBER";?>" maxlength="25" disabled>
-<?php
-if($bEnableVirtualKeyboard === true)
-{
-echo '
-				<script type="text/javascript">
-				$(\'#licenseplateinternational\')
-	.keyboard({ layout: \'alphasmall\', autoAccept: true, tabNavigation: true, enterNavigation: true })
-	.addTyping();
-	</script>
-';
-}
-?>
-			</div>
-		</div>
-		<div class="<?php if($bAllowVisitorsToOptionallyEnterTheirEMailAddress === true) {echo 'table-row';} else {echo 'hide';} ?>">
-			<div class="table-cell-right-align">
-				<?php echo "$TEXT_EMAIL_ADDRESS"?>
-			</div>
-			<div class="table-cell-left-align">
-				<input id="emailinternational" type="text" name="email" autocomplete="off" placeholder="<?php echo "$TEXT_ENTER_YOUR_EMAIL_ADDRESS";?>" maxlength="255">
-<?php
-if($bEnableVirtualKeyboard === true)
-{
-echo '
-				<script type="text/javascript">
-				$(\'#emailinternational\')
-	.keyboard({ layout: \'alphasmall\', autoAccept: true, tabNavigation: true, enterNavigation: true })
-	.addTyping();
-	</script>
-';
-}
-?>
-			</div>
+		<div class="table-cell-left-align">
+			<input required id="nameinternational" type="text" name="name" autocomplete="off" placeholder="<?php echo "$TEXT_ENTER_YOUR_FULL_NAME";?>" maxlength="255">
 		</div>
 	</div>
-	<p>
-	<input type="submit" name="login" value="<?php echo "$TEXT_SIGN_IN_AND_PRINT_VISITOR_BADGE" ?>" OnClick="return checkWholeForm(newvisitor);">
-	<input type="reset" value="<?php echo "$TEXT_CLEAR" ?>">
-	</p>
-	</form>
-	<div class="centered-flow-block">
-	<p class="note">
-		<?php echo $TEXT_NOTE ?> : <?php echo $TEXT_FIELDS_IN_BOLD_ARE_MANDATORY ?>
-	</p>
+	<div class="table-row">
+		<div class="table-cell-right-align">
+			<b><?php echo "$TEXT_NUMBER" ?></b>
+		</div>
+		<div class="table-cell-left-align">
+			<input required id="numberinternational" type="number" min="0" inputmode="numeric" pattern="[0-9]*" name="number" autocomplete="off" placeholder="<?php echo "$TEXT_ENTER_YOUR_NUMBER";?>" maxlength="10">
+		</div>
 	</div>
+	<div class="table-row">
+		<div class="table-cell-right-align">
+			<b><?php echo "$TEXT_VISITOR_TYPE" ?></b>
+		</div>
+		<div class="table-cell-left-align types">
+			<input value="member" type="radio" name="type" required id="member" />
+			<label for="member" class="type">
+				<?php echo "$TEXT_VISITOR_TYPE_MEMBER" ?>
+			</label>
+			<input value="casual user" type="radio" name="type" id="casual_user" />
+			<label class="type" for="casual_user">
+				<?php echo "$TEXT_VISITOR_TYPE_CASUAL_USER" ?>
+			</label>
+			<input value="guest user" type="radio" name="type" id="guest_user" />
+			<label class="type" for="guest_user">
+				<?php echo "$TEXT_VISITOR_TYPE_GUEST_USER" ?>
+			</label>
+			<input value="volunteer" type="radio" name="type" id="volunteer" />
+			<label class="type" for="volunteer">
+				<?php echo "$TEXT_VISITOR_TYPE_VOLUNTEER" ?>
+			</label>
+		</div>
 	</div>
+	<div class="actions">
+		<button type="reset"><?php echo "$TEXT_CLEAR" ?></button>
+		<button type="submit" name="login" OnClick="return checkWholeForm(newvisitor);"><?php echo "$TEXT_SIGN_IN" ?></button>
+	</div>
+</form>
 	</div>
 	</div>
 	<h3><?php echo $TEXT_EXIT ?></h3>
 	<div>
-	<div class="centered-flow">
 	<div class="centered-flow-block">
 	<form name="leave">
 	<p>
@@ -486,24 +236,6 @@ echo '
 	</p>
 	</div>
 	</div>
-	</div>
 </div>
-<p class="copyright">
-	T.C.E.D.I. Open Visitors Management System <?php echo $appVer ?>
-	<br>
-	Copyright &copy; 2016 by T.C.E.D.I. (Jean-Denis Tenaerts)
-	<br>
-<?php
-if($bShowLinkToAdministrationPage === true)
-{
-  echo "<a href=\"admin.php?language=$sLanguage\">$TEXT_ADMINISTRATION</a>&emsp;"."\n";
-}
-if($bShowLinkToReceptionViewPage === true)
-{
-  echo "<a href=\"reception-view.php?language=$sLanguage\">$TEXT_RECEPTION_VIEW</a>&emsp;"."\n";
-}
-?>
-	<a href="about.php?language=<?php echo $sLanguage ?>"><?php echo $TEXT_ABOUT ?></a>
-</p>
 </body>
 </html>
