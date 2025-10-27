9e6ae9d8e55d937b390bd9c78ceb64b924bcdf8f
gc256 <git_cowboy@proton.me>
2025-10-27 17:23:28 +0100

    Add files via upload
    German translation

---
+++ languages/Deutsch/admin.php
@@ -0,0 +1,68 @@
+<?php
+/*
+   T.C.E.D.I. Open Visitors Management System
+   Copyright (c) 2016 by T.C.E.D.I. (Jean-Denis Tenaerts)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+//German translation for admin.php.
+
+$TEXT_TCEDI_OPEN_VISITORS_MANAGEMENT_SYSTEM = 'T.C.E.D.I. Open Visitors Management System';
+$TEXT_ADMINISTRATION_PAGE = 'Administrationsseite';
+$TEXT_REPORTS = 'Berichte';
+$TEXT_DATA_BASE_ADMINISTRATION = 'Datenbankadministration';
+$TEXT_GO_TO = 'Springe zur:';
+$TEXT_MAIN_PAGE = 'Besucherseite';
+$TEXT_RECEPTION_VIEW_PAGE = 'Sicht Empfang';
+$TEXT_REPORT_RUN_ON = 'Bericht ausführen:';
+$TEXT_REFRESH = 'Auffrischen';
+$TEXT_QUERY = 'Abfrage:';
+$TEXT_ONE_ERROR_HAS_OCCURED_WHILE_RUNNING_THE_REPORT_QUERY = 'Beim Ausführen der Berichtsabfrage ist ein Fehler aufgetreten.';
+$TEXT_VISITOR_ID = 'Besucher ID';
+$TEXT_LOCATION = 'Standort';
+$TEXT_FIRST_NAME = 'Vorname';
+$TEXT_LAST_NAME = 'Nachname';
+$TEXT_COMPANY_NAME = 'Firma';
+$TEXT_VISIT_REASON = 'Besuchsgrund';
+$TEXT_EMAIL = 'E-Mail';
+$TEXT_VEHICLE_ON_SITE = 'Fahrzeug vor Ort';
+$TEXT_LICENSE_PLATE = 'Kennzeichen';
+$TEXT_HAS_SIGNED = 'Unterschrieben';
+$TEXT_ARRIVAL = 'Ankunft';
+$TEXT_DEPARTURE = 'Check-out';
+$TEXT_GONE = 'Gone';
+$TEXT_NOTE_ABOUT_INPUT_VALIDATION = 'Hinweis: Drücken Sie die Tab-Taste, um Ihre Eingabe zu bestätigen, oder klicken Sie in eine leere Stelle außerhalb des Feldes, das Sie ausgefüllt haben (der Bericht wird dann automatisch aktualisiert).';
+$TEXT_DATES = 'Tage';
+$TEXT_ALL_DATES = 'Alle Tage';
+$TEXT_SINGLE_DAY = 'Einzelner Tag';
+$TEXT_DATE = 'Datum:';
+$TEXT_SINGLE_DAY_HINT = 'z.B. jj-mm-tt:';
+$TEXT_DATE_RANGE = 'Datumsbereich';
+$TEXT_FROM = 'Von:';
+$TEXT_TO = 'Bis:';
+$TEXT_MAX_ROWS_TO_DISPLAY = 'Maximale Anzahl Zeilen für die Anzeige';
+$TEXT_ERROR_NON_NUMERIC_VALUE = 'Fehler: Sie haben versucht, einen nicht numerischen Wert in das System einzufügen!';
+$TEXT_ERROR_NON_INTEGER_VALUE = 'Fehler: Sie haben versucht, einen nicht ganzzahligen Wert in das System einzufügen.!';
+$TEXT_VISITORS_IN_FACILITY = 'Besucher am Standort';
+$TEXT_ALL_VISITORS = 'Alle Besucher';
+$TEXT_ADMIN_SETTINGS_WARNING = 'Admin settings here can be dangerous. With the exception of the "Set all check out flags" option, other settings could cause data loss. It is suggested to ensure that proper security is applied to your operational database user account. Removing the DELETE privilege for that database user is usually a good practice. All commands of the administration page (admin.php) can be faked with a POST request. So, if you don\'t remove the DELETE privilege for the database user, you should ensure that access to "admin.php" is restricted to authorized computers and that basic authentication is also required.';
+$TEXT_ERROR_CANNOT_DELETE_TABLE_CONTENT = 'Fehler: Tabelleninhalt kann nicht gelöscht werden! Details:';
+$TEST_ERROR_CANNOT_RESET_AUTO_INCREMENT_COUNTER = 'Fehler: Es ist nicht möglich, den Auto-Inkrement-Zähler der Tabelle zurückzusetzen! Details:';
+$TEXT_ERROR_IMPOSSIBLE_TO_SET_ALL_CHECK_OUT_FLAGS = 'Fehler: Es ist unmöglich, alle Check-out-Flags zu setzen! Details:';
+$TEXT_SET_ALL_CHECK_OUT_FLAGS = 'Setzen Sie alle "Check-out"-Flags (alle verlassen den Standort)';
+$TEXT_DELETE_ALL_RECORDS_AND_RESET_AUTO_INCREMENT_COUNTER = 'Löschen Sie alle Datensätze aus der Datenbank und setzen Sie den Auto-Inkrement-Zähler zurück (ACHTUNG: Dadurch wird der gesamte Inhalt der Tabellen entfernt)';
+$TEXT_APPLY_CHANGES = 'Änderungen übernehmen (kann nicht abgebrochen werden)';
+$TEXT_RESET_SELECTIONS_ABOVE = 'Auswahl oben zurücksetzen';
+$TEXT_CHOOSE_YOUR_LANGUAGE = 'Wählen Sie Ihre Sprache:';
+?>
---
+++ languages/Deutsch/main.php
@@ -0,0 +1,69 @@
+<?php
+/*
+   T.C.E.D.I. Open Visitors Management System
+   Copyright (c) 2016-2017 by T.C.E.D.I. (Jean-Denis Tenaerts)
+
+   T.C.E.D.I. Open Visitors Management System is a derivative work based on phpVisitorBadge Enhanced.
+   phpVisitorBadge Enhanced
+   Copyright (c) 2010-2016 RKW ACE S.A.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+//German translation for main.php.
+
+$TEXT_VISITOR_BADGE_TITLE = 'T.C.E.D.I. Open Visitors Management System';
+$TEXT_JAVASCRIPT_SUPPORT_REQUIRED = 'Ihr Browser unterstützt JavaScript nicht oder die JavaScript-Unterstützung wurde deaktiviert. Sie müssen JavaScript aktivieren, um diese Anwendung ordnungsgemäß verwenden zu können.';
+$TEXT_WELCOME_TO = 'Herzlich willkommen bei';
+$TEXT_VISITOR_PLEASE_SIGN_IN = 'Besucher, bitte melden Sie sich an';
+$TEXT_CHOOSE_YOUR_LANGUAGE = 'Wählen Sie Ihre Sprache :';
+$TEXT_FULL_NAME = 'Name (Vor- und Nachname)';
+$TEXT_FIRST_NAME = 'Vorname';
+$TEXT_LAST_NAME = 'Nachname';
+$TEXT_ENTER_YOUR_FIRST_NAME = 'Geben Sie Ihren Vornamen ein...';
+$TEXT_ENTER_YOUR_LAST_NAME = 'Geben Sie Ihren Nachnamen ein...';
+$TEXT_ENTER_YOUR_COMPANY_NAME = 'Geben Sie Ihren Firmennamen ein...';
+$TEXT_ENTER_REASON_FOR_VISIT = 'Geben Sie den Grund für den Besuch ein...';
+$TEXT_ENTER_YOUR_LICENSE_PLATE_NUMBER = 'Ihr Kfz-Kennzeichen...';
+$TEXT_ENTER_YOUR_EMAIL_ADDRESS = 'Geben Sie Ihre E-Mail-Adresse ein...';
+$TEXT_PLEASE_ENTER_YOUR_FIRST_NAME = 'Bitte geben Sie Ihren Vornamen ein.';
+$TEXT_PLEASE_ENTER_YOUR_LAST_NAME = 'Bitte geben Sie Ihren Nachnamen ein.';
+$TEXT_PLEASE_ENTER_YOUR_COMPANY_NAME = 'Bitte geben Sie den Namen Ihres Unternehmens ein.';
+$TEXT_PLEASE_ENTER_REASON_FOR_VISIT = 'Bitte geben Sie den Grund Ihres Besuchs oder den Namen des Mitarbeiters an, den Sie besuchen.';
+$TEXT_VEHICLE_ON_SITE = 'Ist Ihr Fahrzeug vor Ort geparkt?';
+$TEXT_YES = 'Ja';
+$TEXT_NO = 'Nein';
+$TEXT_LICENSE_PLATE_NUMBER = 'Kfz-Kennzeichen';
+$TEXT_PLEASE_INDICATE_WHETHER_YOUR_VEHICLE_IS_ON_THE_SITE = 'Bitte geben Sie an, ob Ihr Fahrzeug auf dem Gelände geparkt ist oder nicht.';
+$TEXT_PLEASE_ENTER_YOUR_LICENSE_PLATE_NUMBER = 'Bitte geben Sie Ihr Kennzeichen ein.';
+// BEGIN Joomla contacts support specific section.
+$TEXT_EMPLOYEE_OR_SERVICE_CONCERNED = 'Mitarbeiter oder Dienststelle, die von Ihrem Besuch betroffen sind';
+$TEXT_PLEASE_SELECT_EMPLOYEE_OR_SERVICE_CONCERNED = 'Bitte wählen Sie den Namen des Mitarbeiters oder die Dienstleistung aus, die von Ihrem Besuch betroffen ist.';
+$TEXT_PLEASE_SELECT = 'Bitte auswählen';
+// END Joomla contacts support specific section.
+$TEXT_COMPANY = 'Firma';
+$TEXT_REASON_FOR_VISIT = 'Grund des Besuchs';
+$TEXT_EMAIL_ADDRESS = 'E-Mail-Adresse';
+$TEXT_SIGN_IN_AND_PRINT_VISITOR_BADGE = 'Anmelden und Besucherausweis ausdrucken';
+$TEXT_SIGN_OUT_LEAVE = 'Abmelden / Verlassen';
+$TEXT_CLEAR = 'Zurücksetzen';
+$TEXT_ADMINISTRATION = 'Administration';
+$TEXT_NOTE = 'Anmerkung';
+$TEXT_FIELDS_IN_BOLD_ARE_MANDATORY = 'Fett gedruckte Felder sind Pflichtfelder.';
+$TEXT_TO_SIGN_OUT_LEAVE_YOU_DONT_HAVE_TO_FILL_IN_THE_FORM = 'An "'.$TEXT_SIGN_OUT_LEAVE.'", Sie müssen das obige Anmeldeformular nicht ausfüllen.';
+$TEXT_ENTRANCE = 'Eingang';
+$TEXT_EXIT = 'Ausgang';
+$TEXT_RECEPTION_VIEW = 'Sicht Empfang';
+$TEXT_ABOUT = 'Über...';
+$TEXT_PLEASE_CHECK_YOUR_EMAIL_ADDRESS = 'Bitte überprüfen Sie Ihre E-Mail-Adresse. Sie scheint ungültig zu sein.';
+?>
